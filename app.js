ce000d97ed683c47c63e09fe6fe8722c798180fa
samuelzubin <szubin06@gmail.com>
2025-09-09 12:39:11 -0700

    Fixed css grid for variable screen size

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const images = [
     {desc: "Inspiration Point", src: "images/DJI_0296.JPG"},
     {desc: "Point Dume", src: "images/DJI_0266.JPG"},
     {desc: "Hollywood", src: "images/DJI_0165.JPG"},
-    {desc: "Santa Monica Pier", src: "images/DJI_0150.JPG"}
+    {desc: "Santa Monica Pier", src: "images/DJI_0150.JPG"},
+    {desc: "Lake Hollywood Park", src: "images/DJI_0161.JPG"},
+    {desc: "Oak Canyon Park", src: "images/DJI_0209.JPG"}
 ];
 
 const gallery = document.getElementById("gallery");
@@ -11,8 +13,8 @@ images.forEach(img => {
   const div = document.createElement("div");
   div.classList.add("image");
   div.innerHTML = `
-    <img src="${img.src}" alt="${img.desc}">
-    <div class="image-description">${img.desc}</div>
-    `;
-    gallery.appendChild(div);
+  <img src="${img.src}" alt="${img.desc}">
+  <div class="image-description">${img.desc}</div>
+  `;
+  gallery.appendChild(div);
 });
